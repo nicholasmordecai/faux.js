03f48ac7a1b929ee3f172500f7d7d0d2141832fb
Nicholas Mordecai <nicholasmordecai@gmail.com>
2022-09-07 23:04:16 +0100

    removed index files as they are now generated on the fly

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Person, PersonOptions } from './factory';
-import { Student, StudentOptions } from './factory';
-export {
-    Person,
-    PersonOptions,
-    Student,
-    StudentOptions,
-}
