6d3818f61993bda0a98df81d71197017b9c965a2
Nicholas Mordecai <nicholasmordecai@gmail.com>
2022-09-09 19:24:27 +0100

    nested arrays and array generics working

--- dev/types/file-one.ts
+++ dev/types/file-one.ts
@@ -1,4 +1,8 @@
 export interface Person {
     name: string;
     age: number;
+    scores: Array<Array<number>>;
+    that: string[];
+    foo: boolean[];
+    // thing: [number, string, boolean];
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,4 @@
-import { PropertySignature, SyntaxKind } from 'ts-morph';
+import { PropertySignature, SyntaxKind, ts, Type } from 'ts-morph';
 
 export function traverseProperty(property: PropertySignature) {
 	return getNativeType(property);
@@ -27,12 +27,20 @@ export function getNativeType(property: PropertySignature) {
 	}
 
 	if(type.isArray()) {
-		return '[1, 2, 3, 4, 5]';
+		console.log('is array...');
+		const arrayType = type.getArrayElementType();
+		if(!arrayType) return '[]';
+		
+		return `[${getNativeFromType(arrayType)}]`;
+	}
+
+	if(type.isTuple()) {
+		console.log('is tuple');
 	}
 
 	// should probably re-write this using the writer.object class instead of constructing strings.
 	if(type.isObject()) {
-		const typeLiteral = property.getLastChildByKind(SyntaxKind.TypeLiteral);
+		const typeLiteral = property.getFirstChildByKind(SyntaxKind.TypeLiteral);
 		if(!typeLiteral) return;
 		const properties = typeLiteral.getProperties();
 		let t = '{';
@@ -54,4 +62,34 @@ export function getNativeType(property: PropertySignature) {
 	// 	t += '}';
 	// 	return t;
 	// }
+}
+
+export function getNativeFromType(type: Type<ts.Type>): any {
+	if(type.isString()) {
+		return '""';
+	}
+
+	if(type.isNumber()) {
+		return 1;
+	}
+
+	if(type.isBoolean()) {
+		return true;
+	}
+
+	if(type.isNull()) {
+		return null;
+	}
+
+	if(type.isUnknown()) {
+		return;
+	}
+
+	if(type.isArray()) {
+		console.log('is array...');
+		const arrayType = type.getArrayElementType();
+		if(!arrayType) return '[]';
+		
+		return `[${getNativeFromType(arrayType)}]`;
+	}
 }
