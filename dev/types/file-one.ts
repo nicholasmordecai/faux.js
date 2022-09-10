723d9c471081fbfdfe9e816f3d4c55cb5db8881c
Nicholas Mordecai <nicholasmordecai@gmail.com>
2022-09-10 12:50:54 +0100

    got array complex generic working

--- dev/types/file-one.ts
+++ dev/types/file-one.ts
@@ -1,18 +1,18 @@
 export interface Person {
-    name: string;
-    age: number;
+    // name: string;
+    // age: number;
     // bool: boolean;
     // nameObj: {
     //     first: string;
     //     last: string;
     // }
     // that: string[];
-    // scores: Array<{name: string}>;
+    scores: Array<{name: string}>;
     // thing: [number, string, {name: string}];
-    address: Address;
+    // address: Address;
 }
 
-interface Address {
-    streetName: string;
-    houseNumber: number;
-}
+// interface Address {
+//     streetName: string;
+//     houseNumber: number;
+// }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,4 @@
-import { Node, PropertySignature, SyntaxKind } from 'ts-morph';
+import { ArrayTypeNode, Node, PropertySignature, SyntaxKind, TypeLiteralNode } from 'ts-morph';
 
 export function traverseProperty(property: PropertySignature) {
 	return traverseNodes(property);
@@ -27,28 +27,21 @@ function traverseNodes(node: Node): any {
 	}
 
 	if (type.isArray()) {
-		const arrayType = type.getArrayElementType();
-		if(!arrayType) return '[]';
-		let s = '[';
-		// const n = arrayType.get
-			// s += traverseNodes(arrayType);
-		s += ']';
-		return s;
+		return handleArrayType(node);
 	}
 
 	if (type.isObject()) {
-	/**
-	 * Interfaces are appearing as objects here
-	 * Need to get 
-	 */
-		console.log('its an object?');
+		if(node.isKind(SyntaxKind.TypeLiteral)) {
+			return loopLiteralType(node);
+		}
+		
 		const nodeChild = node.getFirstChildByKind(SyntaxKind.TypeLiteral);
 		if(!nodeChild) {
 			const symbol = type.getSymbol();
-			
+
 			if(!symbol) return '{}';
 			const identifier = symbol.getName();
-			
+			console.log('got into here')
 			const referenceInterface = node.getSourceFile().getInterface(identifier);
 			if(!referenceInterface) return '{}';
 
@@ -60,12 +53,7 @@ function traverseNodes(node: Node): any {
 			return s;
 		}
 
-		let s = '{';
-		nodeChild.getProperties().forEach((property) => {
-			s += `${property.getName()}: ${traverseNodes(property)},`;
-		});
-		s += '}';
-		return s;
+		return loopLiteralType(nodeChild);
 	}
 
 	if(type.isTuple()) {
@@ -83,156 +71,40 @@ function traverseNodes(node: Node): any {
 	}
 }
 
-// export function getNativeFromType(type: Type<ts.Type>): any {
-// 	if(type.isString()) {
-// 		return '""';
-// 	}
+function handleArrayType(node: Node) {
+	// handles cases like string[]
+	const arrayTypeNode = node.getFirstChildByKind(SyntaxKind.ArrayType);
+	if(arrayTypeNode) {
+		return loopArrayType(arrayTypeNode);
+	}
 
-// 	if(type.isNumber()) {
-// 		return 1;
-// 	}
+	// handles cases like Array<string>
+	const typeReference = node.getFirstChildByKind(SyntaxKind.TypeReference);
+	if(typeReference) {
+		let s = '[';
+		const typeArguments = typeReference.getTypeArguments();
+		typeArguments.forEach((arg) => {			
+			s += traverseNodes(arg);
+		});
+		s += ']';
+		return s;
+	}
+}
 
-// 	if(type.isBoolean()) {
-// 		return true;
-// 	}
+function loopArrayType(arrayTypeNode: ArrayTypeNode) {
+	let s = '[';
+	arrayTypeNode.forEachChild((childNode) => {
+		s += traverseNodes(childNode);
+	})
+	s += ']';
+	return s;
+}
 
-// 	if(type.isNull()) {
-// 		return null;
-// 	}
-
-// 	if(type.isUnknown()) {
-// 		return;
-// 	}
-
-// 	if(type.isArray()) {
-// 		const arrayType = type.getArrayElementType();
-// 		if(!arrayType) return '[]';
-
-// 		return `[${getNativeFromType(arrayType)}]`;
-// 	}
-
-// 	if(type.isObject()) {
-
-// 		// type.getProperties().forEach((symbol) => {
-
-// 		// })
-// 	}
-// }
-
-// function getNativeFromSymbol(symbol: Symbol): any {
-// 	const type = symbol.getDeclaredType();
-// 	if(type.isString()) {
-// 		return '""';
-// 	}
-
-// 	if(type.isNumber()) {
-// 		return 1;
-// 	}
-
-// 	if(type.isBoolean()) {
-// 		return true;
-// 	}
-
-// 	if(type.isNull()) {
-// 		return null;
-// 	}
-
-// 	if(type.isUnknown()) {
-// 		return;
-// 	}
-
-// 	if(type.isArray()) {
-// 		const arrayType = type.getArrayElementType();
-// 		if(!arrayType) return '[]';
-// 		const aTSymbol = arrayType.getSymbol();
-// 		if(!aTSymbol) return '[]';
-// 		return `[${getNativeFromSymbol(aTSymbol)}]`;
-// 	}
-
-// 	if(type.isTuple()) {
-// 		let s = '[';
-// 		type.getTupleElements().forEach((tupleType) => {
-// 			const tupleSymbol = tupleType.getSymbol();
-// 			if(!tupleSymbol) return '[]';
-// 			s += `${getNativeFromSymbol(tupleSymbol)},`;
-// 		});
-// 		s += ']'
-// 		return s;
-// 	}
-
-// 	if(type.isObject()) {
-// 		console.log(type.getProperties());
-// 		let s = '{';
-// 		type.getProperties().forEach((symbol) => {
-// 			s += `${getNativeFromSymbol(symbol)},`;
-// 		});
-// 		s += '}';
-// 		return s;
-// 	}
-// }
-
-// export function getNativeType(property: PropertySignature) {
-// 	const type = property.getType();
-// 	if(type.isString()) {
-// 		return '""';
-// 	}
-
-// 	if(type.isNumber()) {
-// 		return 1;
-// 	}
-
-// 	if(type.isBoolean()) {
-// 		return true;
-// 	}
-
-// 	if(type.isNull()) {
-// 		return null;
-// 	}
-
-// 	if(type.isUnknown()) {
-// 		return;
-// 	}
-
-// 	if(type.isArray()) {
-// 		// console.log('is array...');
-// 		const arrayType = type.getArrayElementType();
-// 		if(!arrayType) return '[]';
-
-// 		return `[${getNativeFromType(arrayType)}]`;
-// 	}
-
-// 	if(type.isTuple()) {
-// 		// console.log('is tuple');
-// 		let s = '[';
-// 		type.getTupleElements().forEach((tupleType) => {
-// 			s += `${getNativeFromType(tupleType)},`;
-// 		});
-// 		s += ']'
-// 		return s;
-// 	}
-
-// 	// should probably re-write this using the writer.object class instead of constructing strings.
-// 	if(type.isObject()) {
-// 		const typeLiteral = property.getFirstChildByKind(SyntaxKind.TypeLiteral);
-// 		if(!typeLiteral) return;
-// 		const properties = typeLiteral.getProperties();
-// 		let t = '{';
-// 		for(const tProperty of properties) {
-// 			t += `${tProperty.getName()}: ${traverseProperty(tProperty)},`;
-// 		}
-// 		t += '}';
-// 		return t;
-// 	}
-
-// 	// if(type.isInterface()) {
-// 	// 	const typeLiteral = property.getLastChildByKind(SyntaxKind.TypeLiteral);
-// 	// 	if(!typeLiteral) return;
-// 	// 	const properties = typeLiteral.getProperties();
-// 	// 	let t = '{';
-// 	// 	for(const tProperty of properties) {
-
-// 	// 	}
-// 	// 	t += '}';
-// 	// 	return t;
-// 	// }
-// }
+function loopLiteralType(literalNode: TypeLiteralNode) {
+	let s = '{';
+	literalNode.getProperties().forEach((property) => {
+		s += `${property.getName()}: ${traverseNodes(property)},`;
+	});
+	s += '}';
+	return s;
+}
