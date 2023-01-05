e76a099edcb3b8b44961f24c6e9728de9aa0e658
Nicholas Mordecai <mordecai.nicholas@gene.com>
2023-01-05 16:39:59 +0000

    test doc generation - still not working right

--- src/generators/geographic/map.ts
+++ src/generators/geographic/map.ts
@@ -1,11 +1,37 @@
 import { rngFloat } from "../math/number";
 
+// Note: Precision is done mathematically not using a string, so unnessessary 0's will be removed from the end.
+// If you want to fix this, you can do: lat().toPrecision(9) and it will return a string representation including trailing 0's.
+
+// todo give a starting point and give a maximum distance from and to
+
+/**
+ * Interface for a position object (latitude and longitude)
+ *
+ * @interface LatLong
+ * @typedef {LatLong}
+ */
 interface LatLong {
+    /**
+     * Latitude
+     *
+     * @type {number}
+     */
     lat: number;
+    /**
+     * Longitude
+     *
+     * @type {number}
+     */
     long: number;
 }
 
-// give a starting point and give a maximum distance from and to
+/**
+ * Generates a latitude and longitude object
+ *
+ * @export
+ * @returns {LatLong}
+ */
 export function LatLong(): LatLong {
     return {
         lat: lat(),
@@ -13,13 +39,22 @@ export function LatLong(): LatLong {
     }
 }
 
-// add start position and distace
+/**
+ * Generate a latitude
+ *
+ * @export
+ * @returns {number}
+ */
 export function lat(): number {
-    return rngFloat({ min: -180, max: 180, precision: 9});
-}
-
-// add start position and distace
-export function long(): number {
     return rngFloat({ min: -180, max: 180, precision: 9 });
 }
 
+/**
+ * Generated a longitude
+ *
+ * @export
+ * @returns {number}
+ */
+export function long(): number {
+    return rngFloat({ min: -180, max: 180, precision: 9 });
+}
--- test/generators/geo/map.spec.ts
+++ test/generators/geo/map.spec.ts
@@ -2,22 +2,20 @@ import { expect } from "chai";
 import { long, lat, LatLong } from './../../../src/generators/geographic/map';
 import { countDecimals } from './../../utils/internal';
 
-//! Sometimes failing due to decimal length being 8 not 9 (due to last decimal being a 0 I think)
-
-describe('Address module', () => {
+describe('Map module', () => {
     it('Should create a random long', () => {
         const lng = long();
         expect(typeof lng).to.be.equal('number');
         expect(lng).to.be.within(-180, 180);
-        expect(countDecimals(lng)).to.be.equal(9);
-
+        console.log(lng);
+        expect(countDecimals(lng)).to.be.lessThanOrEqual(9);
     });
 
     it('Should create a random lat', () => {
         const lt = lat();
         expect(typeof lt).to.be.equal('number');
         expect(lt).to.be.within(-180, 180);
-        expect(countDecimals(lt)).to.be.equal(9);
+        expect(countDecimals(lt)).to.be.lessThanOrEqual(9);
     });
 
     it('Should create a lat long object', () => {
@@ -26,9 +24,9 @@ describe('Address module', () => {
         expect(latLng).to.haveOwnProperty('long');
 
         expect(latLng.lat).to.be.within(-180, 180);
-        expect(countDecimals(latLng.lat)).to.be.equal(9);
+        expect(countDecimals(latLng.lat)).to.be.lessThanOrEqual(9);
 
         expect(latLng.long).to.be.within(-180, 180);
-        expect(countDecimals(latLng.long)).to.be.equal(9);
+        expect(countDecimals(latLng.long)).to.be.lessThanOrEqual(9);
     })
 });
